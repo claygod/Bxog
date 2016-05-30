cc8b5c9f746d44c460d438003138682f8eae8791
Eduard <claygod@yandex.ru>
2016-05-30 11:50:27 +0300

    Change the variable name

--- index.go
+++ index.go
@@ -10,50 +10,50 @@ import (
 
 // Router using the index selects the route
 type index struct {
-	tree  map[type_hash]*node
-	index map[type_hash]*route
+	tree  map[typeHash]*node
+	index map[typeHash]*route
 }
 
 func newIndex() *index {
 	return &index{
-		tree:  make(map[type_hash]*node),
-		index: make(map[type_hash]*route),
+		tree:  make(map[typeHash]*node),
+		index: make(map[typeHash]*route),
 	}
 }
 
 func (x *index) find(url string, req *http.Request, r *Router) *route {
 	salt := x.genSalt(req.Method)
-	c_hashes := [HTTP_SECTION_COUNT]type_hash{}
-	level := x.genUintSlice(url[1:], salt, &c_hashes)
-	var c_node *node
+	cHashes := [HTTP_SECTION_COUNT]typeHash{}
+	level := x.genUintSlice(url[1:], salt, &cHashes)
+	var cNode *node
 
-	if x.tree[c_hashes[0]] != nil {
-		c_node = x.tree[c_hashes[0]]
+	if x.tree[cHashes[0]] != nil {
+		cNode = x.tree[cHashes[0]]
 	} else if x.tree[x.genUint(DELIMITER_STRING, salt)] != nil {
-		c_node = x.tree[x.genUint(DELIMITER_STRING, salt)]
+		cNode = x.tree[x.genUint(DELIMITER_STRING, salt)]
 	} else {
 		return nil
 	}
 	// slash
 	if level == 0 {
-		if c_node.route != nil {
-			return c_node.route
+		if cNode.route != nil {
+			return cNode.route
 		} else {
 			return nil
 		}
 	}
 
 	for i := 0; i < level; i++ {
-		if c_node.route == nil {
-			if c_node.child[c_hashes[i]] != nil {
-				c_node = c_node.child[c_hashes[i]]
-			} else if c_node.child[DELIMITER_UINT] != nil {
-				c_node = c_node.child[DELIMITER_UINT]
+		if cNode.route == nil {
+			if cNode.child[cHashes[i]] != nil {
+				cNode = cNode.child[cHashes[i]]
+			} else if cNode.child[DELIMITER_UINT] != nil {
+				cNode = cNode.child[DELIMITER_UINT]
 			} else {
 				return nil
 			}
 		} else if i == level-1 {
-			return c_node.route
+			return cNode.route
 		} else {
 			return nil
 		}
@@ -66,80 +66,80 @@ func (x *index) compile(routes []*route) {
 		salt := x.genSalt(route.method)
 		x.index[x.genUint(route.id, 0)] = route
 		length := len(route.sections)
-		c_node := newNode()
+		cNode := newNode()
 		// slash
 		if length == 0 {
-			c_node.route = route
-			x.tree[SLASH_HASH] = c_node
+			cNode.route = route
+			x.tree[SLASH_HASH] = cNode
 			continue
 		}
 		c_hash := x.genUint(route.sections[0].id, salt)
 		if x.tree[c_hash] != nil {
-			c_node = x.tree[c_hash]
+			cNode = x.tree[c_hash]
 		} else {
-			switch route.sections[0].type_sec {
+			switch route.sections[0].typeSec {
 			case TYPE_STAT:
-				x.tree[c_hash] = c_node
+				x.tree[c_hash] = cNode
 			case TYPE_ARG:
-				x.tree[x.genUint(DELIMITER_STRING, salt)] = c_node
+				x.tree[x.genUint(DELIMITER_STRING, salt)] = cNode
 			}
 		}
 		for i := 0; i < length; i++ {
 			if i == length-1 {
-				c_node.route = route
+				cNode.route = route
 			} else {
-				new_node := newNode()
-				switch route.sections[i].type_sec {
+				nNode := newNode()
+				switch route.sections[i].typeSec {
 				case TYPE_STAT:
 					if i == 0 {
-						c_node.child[x.genUint(route.sections[i].id, salt)] = new_node
+						cNode.child[x.genUint(route.sections[i].id, salt)] = nNode
 					} else {
-						c_node.child[x.genUint(route.sections[i].id, 0)] = new_node
+						cNode.child[x.genUint(route.sections[i].id, 0)] = nNode
 					}
 				case TYPE_ARG:
 					if i == 0 {
-						c_node.child[x.genUint(DELIMITER_STRING, salt)] = new_node
+						cNode.child[x.genUint(DELIMITER_STRING, salt)] = nNode
 					} else {
-						c_node.child[DELIMITER_UINT] = new_node
+						cNode.child[DELIMITER_UINT] = nNode
 					}
 				}
-				c_node = new_node
+				cNode = nNode
 			}
 		}
 	}
 }
 
-func (x *index) genUintSlice(s string, total type_hash, c_hashes *[HTTP_SECTION_COUNT]type_hash) int {
+func (x *index) genUintSlice(s string, total typeHash, cHashes *[HTTP_SECTION_COUNT]typeHash) int {
 	c := DELIMITER_BYTE
 	na := 0
 	length := len(s)
 	if length == 0 {
-		c_hashes[0] = SLASH_HASH
+		cHashes[0] = SLASH_HASH
 		return 0
 	}
 
 	for i := 0; i < length; i++ {
 		if s[i] == c {
-			c_hashes[na] = total
+			cHashes[na] = total
 			total = 0
 			na++
 			continue
 		}
-		total = total<<5 + type_hash(s[i])
+		total = total<<5 + typeHash(s[i])
 	}
-	c_hashes[na] = total
+	cHashes[na] = total
 	na++
 	return na
 }
 
-func (x *index) genUint(s string, total type_hash) type_hash {
+func (x *index) genUint(s string, total typeHash) typeHash {
 	length := len(s)
 	for i := 0; i < length; i++ {
-		total = total<<5 + type_hash(s[i])
+		total = total<<5 + typeHash(s[i])
 	}
 	return total
 }
 
-func (x *index) genSalt(s string) type_hash {
-	return type_hash(s[0] + s[1])
+func (x *index) genSalt(s string) typeHash {
+	return typeHash(s[0] + s[1])
 }
